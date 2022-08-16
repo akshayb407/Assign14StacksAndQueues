1af34b8417d35fbf5f9a23563e21314f488d3f5d
akshayb407 <akshayb407@gmail.com>
2022-08-16 10:45:52 +0530

    .gitignore

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks And Queues Programs!");
+        }
+    }
+}
