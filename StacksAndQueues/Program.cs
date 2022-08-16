611e7ac326f8f8559e821a97b7de1215f0fd3a88
akshayb407 <akshayb407@gmail.com>
2022-08-16 11:01:20 +0530

    UC1

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -7,6 +7,13 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks And Queues Programs!");
+            Console.WriteLine("Stacks Method");
+            Stack stack = new Stack();
+            stack.Push(10);
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();
         }
     }
 }
---
+++ StacksAndQueues/Stack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Stack
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+        public static Node head;
+
+        public void Push(int data)
+        {
+            Stack stack = new Stack();
+            stack.Add(data);
+        }
+
+        public bool Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (node == null)
+            {
+                head = node;
+                return true;
+            }
+            node.next = head;
+            head = node;
+            return true;
+
+        }
+
+        public void Display()
+        {
+            Node t = head;
+
+            if (t == null)
+            {
+                Console.WriteLine("Node is empty");
+            }
+            while (t.next != null)
+            {
+                Console.WriteLine(t.data);
+                t = t.next;
+            }
+        }
+
+        /* public int peek()
+         {
+             if (head == null)
+                 throw new NullReferenceException("empty List");
+             Node t = head, p = head;
+             while (t.next != null)
+             {
+                 p = t;
+                 t = t.next;
+             }
+             int obj = t.data;
+             return obj;
+         }*/
+
+        /* public int Pop()
+         {
+             if (head == null)
+             {
+                 throw new NullReferenceException("List is Empty");
+             }
+             Node t = head, p = head;
+             while (t.next != null)
+             {
+                 p = t;
+                 t = t.next;
+             }
+            int obj = t.data;
+             p.next = null;
+             return obj;
+         }*/
+    }
+}
