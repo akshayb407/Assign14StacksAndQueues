56eec4b14eba4f2ee384556e1e38dd3753865109
akshayb407 <akshayb407@gmail.com>
2022-08-16 11:55:53 +0530

    UC4

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -12,6 +12,18 @@ namespace StacksAndQueues
             stack.Append(56);
             stack.Append(30);
             stack.Append(70);
+            int element = stack.peek();
+            Console.WriteLine("Peeked element is " + element);
+            int popedElement = stack.Pop();
+            Console.WriteLine("Popped elemment is " + popedElement);
+            int element1 = stack.peek();
+            Console.WriteLine("Peeked element is " + element1);
+            int popedElement1 = stack.Pop();
+            Console.WriteLine("Popped elemment is " + popedElement1);
+            int element2 = stack.peek();
+            Console.WriteLine("Peeked element is " + element2);
+            int popedElement2 = stack.Pop();
+            Console.WriteLine("Popped elemment is " + popedElement2);
             stack.Display();
         }
     }
